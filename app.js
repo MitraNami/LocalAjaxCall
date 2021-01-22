cbd561322bba114f650df9c097080db928a9e13d
Mitra Nami <a.mxnami@gmail.com>
2021-01-22 15:06:41 -0500

    Get the customers from the json file with xhr object and display the info in the DOM

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-document.getElementById('button1').addEventListener('click',loadCustomer );
+document.getElementById('button1').addEventListener('click',loadCustomer);
+
+document.getElementById('button2').addEventListener('click', loadCustomers);
 
 function loadCustomer(evt) {
 
@@ -20,4 +22,34 @@ function loadCustomer(evt) {
     }
   };
   xhr.send()
+}
+
+function loadCustomers(evt) {
+
+  const xhr = new XMLHttpRequest();
+
+  xhr.open('GET', './customers.json', true);
+
+  xhr.onload = function() {
+    if (this.status === 200) {
+      const customers = JSON.parse(this.responseText);
+
+      const output = customers.reduce((acc, customer) => {
+        const {id, name, company, phone} = customer;
+        return acc + `
+          <ul>
+            <li>ID: ${id}</li>
+            <li>Name: ${name}</li>
+            <li>Company: ${company}</li>
+            <li>Phone: ${phone}</li>
+          </ul>
+        `;
+      },'');
+
+      document.querySelector('#customers').innerHTML = output;
+
+    }
+  };
+
+  xhr.send();
 }
